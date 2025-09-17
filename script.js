0d8fbcc42cbca4005b566a661fc244b35c890abb
GabrielLopes <gabrielfcb.2701@gmail.com>
2025-09-16 21:48:31 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const botao = document.getElementById("botão")
-const cores = ["lightblue", "lightpink", "black"];
-const alterar_color = () => {
-    const corAleatoria = cores[Math.floor(Math.random() * cores.length)];
-    document.body.style.backgroundColor = corAleatoria;
-};
-botao.addEventListener("click", () => alterar_color())
-
-
